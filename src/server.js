3811213f50fda0b34f89418cc89c211ba285bb1b
Dallin Parker <13424017+dallin-r-parker@users.noreply.github.com>
2021-08-10 23:51:49 -0500

    :construction: rendering sidebar list

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import { createServer, Model } from "miragejs";
 export default function () {
   createServer({
     models: {
+      list: Model,
       reminder: Model,
     },
 
@@ -11,23 +12,34 @@ export default function () {
       server.create("reminder", { text: "Walk the dog" });
       server.create("reminder", { text: "Take out the trash" });
       server.create("reminder", { text: "Work out" });
+
+      server.create("list", { name: "Home" });
+      server.create("list", { name: "Work" });
     },
 
     routes() {
-      const path = "/api/reminders";
-      this.get(path, (schema) => {
+      // ============= REMINDERS =====================
+      const remindersPath = "/api/reminders";
+      this.get(remindersPath, (schema) => {
         return schema.reminders.all();
       });
 
-      this.post(path, (schema, req) => {
+      this.post(remindersPath, (schema, req) => {
         let attrs = JSON.parse(req.requestBody);
 
         return schema.reminders.create(attrs);
       });
 
-      this.delete(`${path}/:id`, (schema, { params }) =>
+      this.delete(`${remindersPath}/:id`, (schema, { params }) =>
         schema.reminders.find(params.id).destroy()
       );
+      // ============= List =========================
+      const listPath = "/api/lists";
+      this.get(listPath, (schema) => schema.lists.all());
+
+      // this.get("/api/lists", (schema, request) => {
+      //   return schema.lists.all();
+      // });
     },
   });
 }
