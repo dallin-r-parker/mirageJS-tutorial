d742ab23c3882cd67d679348c8946eca86032ba5
Dallin Parker <13424017+dallin-r-parker@users.noreply.github.com>
2021-08-10 22:52:09 -0500

    first route handler to mock static data

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
 // Welcome to the tutorial!
+import { createServer } from "miragejs";
+
+export default function () {
+  createServer({
+    routes() {
+      this.get("/api/reminders", () => ({
+        reminders: [
+          {id: 1, text: 'Walk the dog'},
+          {id: 2, text: 'Walk the cat'},
+          {id: 3, text: 'Walk the dogat'}
+        ]
+      }))
+    }
+  })
+}
