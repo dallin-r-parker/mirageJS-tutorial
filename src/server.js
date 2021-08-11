fa686ba2da52326154fb2a5a50448bfe97f462e9
Dallin Parker <13424017+dallin-r-parker@users.noreply.github.com>
2021-08-11 00:00:32 -0500

    :white_check_mark: created data relationships

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 // Welcome to the tutorial!
-import { createServer, Model } from "miragejs";
+import { createServer, Model, hasMany, belongsTo } from "miragejs";
 
 export default function () {
   createServer({
     models: {
-      list: Model,
-      reminder: Model,
+      list: Model.extend({
+        reminders: hasMany(),
+      }),
+      reminder: Model.extend({
+        list: belongsTo(),
+      }),
     },
 
     seeds(server) {
@@ -13,8 +17,11 @@ export default function () {
       server.create("reminder", { text: "Take out the trash" });
       server.create("reminder", { text: "Work out" });
 
-      server.create("list", { name: "Home" });
-      server.create("list", { name: "Work" });
+      let homeList = server.create("list", { name: "Home" });
+      server.create("reminder", { list: homeList, text: "Do taxes" });
+
+      let workList = server.create("list", { name: "Work" });
+      server.create("reminder", { list: workList, text: "Visit bank" });
     },
 
     routes() {
@@ -37,9 +44,10 @@ export default function () {
       const listPath = "/api/lists";
       this.get(listPath, (schema) => schema.lists.all());
 
-      // this.get("/api/lists", (schema, request) => {
-      //   return schema.lists.all();
-      // });
+      this.get(
+        `${listPath}/:id/reminders`,
+        ({ lists }, { params }) => lists.find(params.id).reminders
+      );
     },
   });
 }
